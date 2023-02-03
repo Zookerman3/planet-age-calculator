eb709f24beffe4757e23d16b70b8d6e03d68646f
Zookerman3 <szook7@gmail.com>
2023-02-03 14:46:31 -0800

    green phase for refactor all functions to methods

--- _tests_/planetobj.test.js
+++ _tests_/planetobj.test.js
@@ -19,25 +19,22 @@ describe("Person", () => {
         expect(person.mercuryAge()).toEqual(41);
     });
 
-    // test("should take the person.age property and calculate the person age on Venus by multiplying the age by 365 then divide that number by 225", () => {
-    //     venusAge();
-    //     expect(venusAge()).toEqual(16);
-    // });
-    // test("should take the person.age property and calculate the person age on Mars by multiplying the age by 365 then divide that number by 687", () => {
-    //     marsAge();
-    //     expect(marsAge()).toEqual(5);
-    // });
+    test("should take the person.age property and calculate the person age on Venus by multiplying the age by 365 then divide that number by 225", () => {
+        
+        expect(person.venusAge()).toEqual(16);
+    });
 
-    // test("should take the person.age property and calculate the person age on Jupiter by multiplying the age by 365 then divide that number by 12", () => {
-    //     jupiterAge();
-    //     expect(jupiterAge()).toEqual(304);
-    // });
+    test("should take the person.age property and calculate the person age on Mars by multiplying the age by 365 then divide that number by 687", () => {
+        expect(person.marsAge()).toEqual(5);
+    });
 
-    // test("should take the person.age property and find the difference between it and the AgeMinusBirthday paramater", () => {
-    //     let person = new Person(23);
-    //     AgeMinusBirthday(7);
-    //     expect(AgeMinusBirthday(7)).toEqual(16);
-    // });
+    test("should take the person.age property and calculate the person age on Jupiter by multiplying the age by 365 then divide that number by 12", () => {
+        expect(person.jupiterAge()).toEqual(304);
+    });
+
+    test("should take the person.age property and find the difference between it and the AgeMinusBirthday paramater", () => {
+        expect(person.ageMinusBirthday(7)).toEqual(3);
+    });
 
 
 
--- src/planetobj.js
+++ src/planetobj.js
@@ -9,26 +9,22 @@ export default class Person {
     }
     
     venusAge(){
-        let person = new Person(10);
-        let vAge = (person.age * 365) / (225)
+        let vAge = (this.age * 365) / (225)
         return Math.floor(vAge);
     }
     
     marsAge(){
-        let person = new Person(10);
-        let marAge = (person.age * 365) / (687)
+        let marAge = (this.age * 365) / (687)
         return Math.floor(marAge);
     }
     
-    jupiterAge(){
-        let person = new Person(10);
-        let jAge = (person.age * 365) / (12)
+    jupiterAge(){ 
+        let jAge = (this.age * 365) / (12)
         return Math.floor(jAge);
     }
     
-    AgeMinusBirthday(pastBirthday){
-        let person = new Person(23)
-        let ageDiff = (person.age - pastBirthday)
+    ageMinusBirthday(pastBirthday){
+        let ageDiff = (this.age - pastBirthday)
         return ageDiff
     }
 
