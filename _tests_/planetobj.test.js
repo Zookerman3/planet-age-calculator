29d0619fb9af3742cabd31bc38bebd733c05929b
Zookerman3 <szook7@gmail.com>
2023-02-03 10:26:07 -0800

    red phase for desciebe("mercuryAge") and test

--- _tests_/planetobj.test.js
+++ _tests_/planetobj.test.js
@@ -7,3 +7,10 @@ describe("Person", () => {
         expect(person.age).toEqual(10);
     });
 });
+
+describe("mercuryAge", () => {
+    test("should take the person.age property and calculate the person age by multiplying the age by 365 then divide that number by 88", () => {
+        let person= new Person(10);
+        expect(person.age).toEqual(41)
+    })
+})
